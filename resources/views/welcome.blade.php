e09bed6d7bb51dbdf0cfdf4eaa135824c5f11dba
pramudyabayu <pramudya.27@students.amikom.ac.id>
2021-12-29 13:56:28 +0700

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -114,6 +114,8 @@
 
           <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5" data-aos="fade-left">
             <h3>Profile POSYANDU</h3>
+            <p>Posyandu merupakan salah satu bentuk Upaya Kesehatan Bersumberdaya Masyarakat (UKBM) yang dikelola dari, oleh, untuk, dan bersama masyarakat, 
+              guna memberdayakan masyarakat dan memberikan kemudahan kepada masyarakat dalam memperoleh pelayanan kesehatan dasar.</p>
             
             <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
               <div class="icon"><i class='bx bx-check'></i></div>
@@ -194,15 +196,14 @@
 
           <div class="col-lg-3 col-md-6">
             <div class="count-box">
-              <i class="bi bi-emoji-smile"></i>
-              
+              <i class='bx bxs-graduation' ></i>
               <p>Meningkatkan kualitas layanan pendidikan</p>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-6 mt-5 mt-md-0">
             <div class="count-box">
-              <i class="bi bi-journal-richtext"></i>
+              <i class='bx bx-line-chart'></i>
               
               <p>Meningkatkan kesejahteraan masyarakat</p>
             </div>
@@ -210,7 +211,7 @@
 
           <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
             <div class="count-box">
-              <i class="bi bi-headset"></i>
+              <i class='bx bxs-tree'></i>
               
               <p>Meningkatkan kelestarian lingkungan hidup</p>
             </div>
@@ -218,7 +219,7 @@
 
           <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
             <div class="count-box">
-              <i class="bi bi-people"></i>
+              <i class='bx bx-plus-medical'></i>
               
               <p>Mengoptimalkan Pelayanan Kesehatan</p>
             </div>
@@ -261,20 +262,8 @@
 
         <div class="faq-list">
           <ul>
-            <li data-aos="fade-up">
-              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1">Apa saja yang perlu dilakukan apabila balita batuk?<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-              <div id="faq-list-1" class="collapse" data-bs-parent=".faq-list">
-                <p>
-                  1. Teruskan pemberian ASI bila bayi masih menyusui. <br>
-                  2. Bila umur anak lebih dari 6 bulan, beri makan dan minuman hangat lebih banyak. <br>
-                  3. Pada anak umur 1 tahun keatas, beri kecap manis ditambah madu atau air jeruk. <br>
-                  4. Bersihkan hidung agar tidak terganggu pernafasannya. <br>
-                  5. Rujuk ke Puskesmas apabila ada tanda-tanda nafas cepat, ada tanda sukar bernafas, dan batuk pilek dengan panas tinggi <br>
-                </p>
-              </div>
-            </li>
 
-            <li data-aos="fade-up" data-aos-delay="100">
+            <li data-aos="fade-up" >
               <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-2" class="collapsed">Apa saja yang perlu dilakukan apabila balita diare?<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
               <div id="faq-list-2" class="collapse" data-bs-parent=".faq-list">
                 <p>
@@ -283,6 +272,19 @@
                   3. Teruskan pemberian makanan. <br>
                   4. Cegah diare dengan cara minum air matang, cuci tangan pakai sabunsebelum dan sesudah makan dan sesudah buang air besar. <br>
                   5. Rujuk ke Puskesmas apabila ada tanda-tanda anak tidak membaik dalam 2 hari, buang air besar encer berkali-kali, muntah berulang ulang, rasa haus yang nyata, demam, makan atau minum sedikit, dan ada darah dalam tinja. <br>
+                </p>
+              </div>
+            </li>
+            
+            <li data-aos="fade-up" data-aos-delay="100">
+              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1">Apa saja yang perlu dilakukan apabila balita batuk?<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="faq-list-1" class="collapse" data-bs-parent=".faq-list">
+                <p>
+                  1. Teruskan pemberian ASI bila bayi masih menyusui. <br>
+                  2. Bila umur anak lebih dari 6 bulan, beri makan dan minuman hangat lebih banyak. <br>
+                  3. Pada anak umur 1 tahun keatas, beri kecap manis ditambah madu atau air jeruk. <br>
+                  4. Bersihkan hidung agar tidak terganggu pernafasannya. <br>
+                  5. Rujuk ke Puskesmas apabila ada tanda-tanda nafas cepat, ada tanda sukar bernafas, dan batuk pilek dengan panas tinggi <br>
                 </p>
               </div>
             </li>
@@ -386,7 +388,7 @@
 
           <div class="col-lg-8 mt-5 mt-lg-0" data-aos="fade-left" data-aos-delay="200">
 
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.758152191752!2d110.59561311420926!3d-7.709080578500931!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a440c245d34e3%3A0x7994aac8b729e0d0!2sDISPERMASDES!5e0!3m2!1sid!2sid!4v1640735169772!5m2!1sid!2sid" width="100%" height="450px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.758152191752!2d110.59561311420926!3d-7.709080578500931!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a440c245d34e3%3A0x7994aac8b729e0d0!2sDISPERMASDES!5e0!3m2!1sid!2sid!4v1640735169772!5m2!1sid!2sid" width="100%" height="300px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
           </div>
 
@@ -399,72 +401,16 @@
 
   <!-- ======= Footer ======= -->
   <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-4 col-md-6">
-            <div class="footer-info">
-              <h3>Posyandu</h3>
-              <p class="pb-3">
-                <em>Posyandu merupakan salah satu bentuk Upaya Kesehatan Bersumberdaya Masyarakat (UKBM) yang dikelola dari, oleh, untuk, dan bersama masyarakat, guna memberdayakan masyarakat dan memberikan kemudahan kepada masyarakat dalam memperoleh pelayanan kesehatan dasar.</em>
-              </p>
-             
-              <div class="social-links mt-3">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-
-          </div>
-
-        </div>
-      </div>
-    </div>
-
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong><span>Bootslander</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>2021</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bootslander-free-bootstrap-landing-page-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        <a href="https://diskominfo.klatenkab.go.id/">Diskominfo Kabupaten Klaten</a>
       </div>
     </div>
   </footer><!-- End Footer -->
